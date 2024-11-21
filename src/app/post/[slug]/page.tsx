dddb9f91155677644fc13ba1f97249df794fb8e6
peterlidee <peter@lidee.be>
2024-11-21 21:05:07 +0100

    Added bug reproduction page

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import { notFound } from 'next/navigation';
+
+// default true
+export const dynamicParams = true;
+
+// [option 1. use generateStaticParams]
+export function generateStaticParams() {
+  return [{ slug: 'a' }];
+}
+
+// [option 2. use dynamic]
+// export const dynamic = 'force-static';
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function page({ params }: Props) {
+  const slug = (await params).slug;
+  const validParams = ['a', 'b'];
+  if (!validParams.includes(slug)) {
+    notFound();
+  }
+  return <div>page slug: {slug}</div>;
+}
